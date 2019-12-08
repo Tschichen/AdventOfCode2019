e66db70ee0cc199d800de6b7fbe03d60b7278949
Christiane <gaertner.christiane@web.de>
2019-12-08 19:22:11 +0100

    day8 unfinished

--- day8/day8.py
+++ day8/day8.py
@@ -4,45 +4,48 @@ import argparse
 
 def build_layers(input):
     layer_array = []
-    k = 0
-    while k*25*6 < len(input)-1:
-        layer_array.append([])
-        for i in range(25):
-            word = ""
-            for j in range(6):
-                char = input[j*k]
-                word += char
-            layer_array[-1].append(word)
+    i = 0
+    while i < len(input)-24:
+        word = input[i:i+25]
+        layer_array.append(word)
+        i += 25
 
-            k += 1
-    return layer_array
+    final_layers = []
+    for i in range (len(layer_array)):
+        if i % 6 == 0:
+            single_layer = []
+            single_layer.append(layer_array[i])
+            final_layers.append(single_layer)
+        else:
+            final_layers[-1].append(layer_array[i])
+
+    return final_layers
 
 def count_numbers(layer_array, number):
     count = 0
     for layer in layer_array:
-        number = sentence.count(number)
-        count += number
+        amount = layer.count(number)
+        count += amount
 
     return count
 
 def find_result(layer_array):
     zeros = []
     for layer in layer_array:
-        number = count_zeros(layer, "0")
+        number = count_numbers(layer, "0")
         zeros.append([number, layer])
 
-    zeros.sort(reverse=True)
+    zeros.sort()
 
-    max_zeros = zeros[0]
-    layers_for_result = max_zeros[1]
+    print(zeros)
+
+    min_zeros = zeros[0]
+    layers_for_result = min_zeros[1]
     einser = count_numbers(layers_for_result, "1")
     zweier = count_numbers(layers_for_result, "2")
     result = einser * zweier
 
     return result
-
-
-
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -59,8 +62,8 @@ if __name__ == "__main__":
 
     if args.part == "part1":
         print("part1")
+        print(len(input_string))
         layer_array = build_layers(input_string)
-        print(layer_array)
         result = find_result(layer_array)
         print(result)
 
