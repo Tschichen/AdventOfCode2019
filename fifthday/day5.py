3780295a94fc769006d7f9dcbf29d5a2d790e50d
Christiane <gaertner.christiane@web.de>
2019-12-08 10:55:12 +0100

    day 5, part1 unfinished

---
+++ fifthday/day5.py
@@ -0,0 +1,66 @@
+import argparse
+
+
+def run_intcode(new_intcode):
+    output = []
+    for i in range(len(new_intcode)-4):
+        entry = str(new_intcode[i])
+        entry_array = []
+        for char in entry:
+            if char != "-":
+                entry_array.append(int(char))
+        #print(entry_array)
+        number = entry_array[-1]
+        if number == 9 and entry_array[-2] == 9 and len(entry_array) == 2:
+            return new_intcode
+        if number == 1 and len(entry_array) > 1 and entry_array[-2] == 0:
+            if len(entry_array) < 3 or entry_array[-3] == 0:
+                first_number = int(new_intcode[int(new_intcode[i+1])])
+            else:
+                first_number = new_intcode[i+1]
+            if len(entry_array) < 4 or entry_array[-4] == 0:
+                second_number = int(new_intcode[int(new_intcode[i+2])])
+            else:
+                second_number = new_intcode[i+2]
+            result = int(first_number) + int(second_number)
+            new_intcode[int(new_intcode[i+3])] = result
+        if number == 2 and len(entry_array) > 1 and entry_array[-2] == 0:
+            if len(entry_array) < 3 or entry_array[-3] == 0:
+                first_number = new_intcode[int(new_intcode[i+1])]
+            else:
+                first_number = new_intcode[i+1]
+            if len(entry_array) < 4 or entry_array[-4] == 0:
+                second_number = new_intcode[int(new_intcode[i+2])]
+            else:
+                second_number = new_intcode[i+2]
+            result = int(first_number) * int(second_number)
+            new_intcode[int(new_intcode[i+3])] = result
+        if number == 3 and len(entry_array) < 2:
+            input = 1
+            new_intcode[int(new_intcode[i+1])] = input
+        if number == 4 and len(entry_array) < 2:
+            output.append(new_intcode[int(new_intcode[i+1])])
+            print(new_intcode[int(new_intcode[i+1])])
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-i', '--infile', required=True)
+    parser.add_argument('-p', '--part', help="enter 'part1' or 'part2'. default: 'part1'", default='part1')
+    args = parser.parse_args()
+
+    input_values = []
+    with open(args.infile) as file:
+        input = file.readlines()
+        line = input[0]
+        values = line.split(',')
+        for entry in values:
+            input_values.append(entry)
+
+    if args.part == "part1":
+        print("part1")
+        code = run_intcode(input_values)
+        #print(code)
+
+    if args.part == "part2":
+        print("part2")
